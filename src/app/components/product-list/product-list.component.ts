8a8d34552ad16d88932a0fd07f419fefdc25e430
Chandra <chandrakiranreddy10@gmail.com>
2024-11-05 17:10:04 -0600

    Landing page changes

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -7,8 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   products = [
-    { name: 'Rose & Lily Bouquet', price: 99, image: 'assets/flower1.jpg' },
-    { name: 'Luxury Orchid Hamper', price: 149, image: 'assets/flower2.jpg' },
-    { name: 'Sunflower Delight', price: 89, image: 'assets/flower3.jpg' },
+    { name: 'Gift Hampers', price: 99, image: 'assets/giftHampers.jpg' },
+    { name: 'Corporate Gifts', price: 149, image: 'assets/corporateGifts.jpg' },
+    { name: 'Engrave Your Story', price: 89, image: 'assets/engravingGifts.jpg' },
+    { name: '{Engrave Your Story}', price: 89, image: 'assets/engravingGifts.jpg' },
+    { name: '{Gift Hampers}', price: 99, image: 'assets/giftHampers.jpg' },
+    { name: '{Corporate Gifts}', price: 149, image: 'assets/corporateGifts.jpg' },
   ];
 }
